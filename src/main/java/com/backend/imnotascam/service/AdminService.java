7ba8b92d3bc35e0caa5c2103f871f5e7e930d98e
DomingoMr <domingo.mrodenas@outlook.es>
2023-05-03 22:45:42 +0200

    Control url entrada

--- src/main/java/com/backend/imnotascam/service/AdminService.java
+++ src/main/java/com/backend/imnotascam/service/AdminService.java
@@ -6,7 +6,11 @@ import com.backend.imnotascam.entity.TWhiteList;
 import com.backend.imnotascam.repository.BlackListRepository;
 import com.backend.imnotascam.repository.WhiteListRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,27 +22,64 @@ public class AdminService {
 
     public Response addToBlackList(String url) {
         Response response = new Response();
-        blackListRepository.save(new TBlackList(url));
 
-        response.setCode("0");
-        response.setData("Url: " + url);
-        response.setDescription("Url anadidia a la lista negra");
+        try {
+            //Compruebo que la url este ya registrada
+            Optional<TBlackList> blackListOptional = blackListRepository.findByUrl(url);
+            if (blackListOptional.isPresent()) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Url ya registrada");
+            }
 
+            //Guardo el registro
+            blackListRepository.save(new TBlackList(url));
+
+            response.setCode("0");
+            response.setData("Url: " + url);
+            response.setDescription("Url anadidia a la lista negra");
+        } catch (ResponseStatusException e) {
+            response.setCode("1");
+            response.setData(e.getMessage());
+            response.setDescription("Error controlado");
+        } catch (Exception e) {
+            response.setCode("100");
+            response.setData(e.getMessage());
+            response.setDescription("Error no controlado");
+        }
 
         return response;
     }
 
     public Response addToWhiteList(String url) {
         Response response = new Response();
-        whiteListRepository.save(new TWhiteList(url));
 
-        response.setCode("0");
-        response.setData("Url: " + url);
-        response.setDescription("Url anadidia a la lista blanca");
+        try {
+            //Compruebo que la url este ya registrada
+            Optional<TWhiteList> whiteListOptional = whiteListRepository.findByUrl(url);
+            if (whiteListOptional.isPresent()) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Url ya registrada");
+            }
 
+            //Guardo el registro
+            whiteListRepository.save(new TWhiteList(url));
 
-        return response;
+            response.setCode("0");
+            response.setData("Url: " + url);
+            response.setDescription("Url anadidia a la lista blanca");
+
+        } catch (ResponseStatusException e) {
+            response.setCode("1");
+            response.setData(e.getMessage());
+            response.setDescription("Error controlado");
+        } catch (Exception e) {
+            response.setCode("100");
+            response.setData(e.getMessage());
+            response.setDescription("Error no controlado");
+        }
+            return response;
+
     }
+
+
     public Response checkUrl(String url) {
         Response response = new Response();
 
